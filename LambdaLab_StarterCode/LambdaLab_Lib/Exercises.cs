1322eb8274a466fb53eb5c7969d39e431012ef87
AidenSpartaGlobal <Skwok@spartaglobal.com>
2023-05-02 17:55:59 +0100

    lab done

--- LambdaLab_StarterCode/LambdaLab_Lib/Exercises.cs
+++ LambdaLab_StarterCode/LambdaLab_Lib/Exercises.cs
@@ -2,46 +2,29 @@
 {
     public class Exercsises
     {
-        public static int CountTotalNumberOfSpartans(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
-        }
+        public static int CountTotalNumberOfSpartans(List<Spartan> spartans) => spartans.Count();
 
-        public static int CountTotalNumberOfSpartansInUKAndUSA(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
-        }
 
-        public static int NumberOfSpartansBornAfter1980(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
-        }
+        public static int CountTotalNumberOfSpartansInUKAndUSA(List<Spartan> spartans) => spartans.Count(x => x.Country == "U.K." || x.Country == "U.S.A.");
+      
 
-        public static int SumOfAllSpartaMarksMoreThan50Inclusive(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
-        }
+        public static int NumberOfSpartansBornAfter1980(List<Spartan> spartans) => spartans.Count(x => x.DateOfBirth.Year > 1980);
+
+
+        public static int SumOfAllSpartaMarksMoreThan50Inclusive(List<Spartan> spartans) => spartans.Where(x => x.SpartaMark >= 50).Select(x => x.SpartaMark).Sum();
 
         //To 2 decimal points
-        public static double AverageSpartanMark(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
-        }
+        public static double AverageSpartanMark(List<Spartan> spartans) => Math.Round(spartans.Average(x => x.SpartaMark), 2);
 
-        public static List<Spartan> SortByAlphabeticallyByLastName(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
 
-        }
 
-        public static List<string> ListAllDistinctCities(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
-        }
+        public static List<Spartan> SortByAlphabeticallyByLastName(List<Spartan> spartans) => spartans.OrderBy(x => x.LastName).ToList();
 
-        public static List<Spartan> ListAllSpartansWithIdBeginingWithB(List<Spartan> spartans)
-        {
-            throw new NotImplementedException();
-        }
+
+        public static List<string> ListAllDistinctCities(List<Spartan> spartans) => spartans.Select(x => x.City).Distinct().ToList();
+
+
+        public static List<Spartan> ListAllSpartansWithIdBeginingWithB(List<Spartan> spartans) => spartans.FindAll(x => x.SpartanId.StartsWith("B"));
+      
     }
 }
