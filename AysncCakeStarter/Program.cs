9f3afeccd0210b7de1dc9f442c5a1ceecb48a686
AidenSpartaGlobal <Skwok@spartaglobal.com>
2023-05-03 11:38:38 +0100

    Async task

--- AysncCakeStarter/Program.cs
+++ AysncCakeStarter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AysncCake
 {
@@ -16,18 +17,47 @@ namespace AysncCake
         private static void HaveAParty()
         {
             var name = "Cathy";
-            var cake = BakeCake();
+            Task<Drinks> drinksTask = GetDrinksAsync();
+            Console.WriteLine($"Guys, I'm back. {drinksTask.Result}.");
+
+            Task<PassOut> PassOutTask = PassOutAsync();
+            Console.WriteLine($"I'm tired. {PassOutTask.Result}.");
+
+            Task<Cake> cakeTask = BakeCakeAsync();
             PlayPartyGames();
             OpenPresents();
-            Console.WriteLine($"Happy birthday, {name}, {cake}!!");
+
+            //Result is 
+            //var cake = cakeTask.Result;
+            Console.WriteLine($"Happy birthday, {name}, {cakeTask.Result}!!");
         }
 
-        private static Cake BakeCake()
+        //We want to get on with other stuff as we
+        //wait for the cake to finish baking
+        //Async methods - name of the method with Async at the end
+        private async static Task<Cake> BakeCakeAsync()
         {
             Console.WriteLine("Cake is in the oven");
-            Thread.Sleep(TimeSpan.FromSeconds(5));
+            //Thread.Sleep(TimeSpan.FromSeconds(5));
+            await Task.Delay(TimeSpan.FromSeconds(5));
             Console.WriteLine("Cake is done");
             return new Cake();
+        }
+
+        private async static Task<Drinks> GetDrinksAsync()
+        {
+            Console.WriteLine("Someone is buying the drinks");
+            await Task.Delay(TimeSpan.FromSeconds(1.5));
+            Console.WriteLine("Someone has returned with fresh water");
+            return new Drinks();
+        }
+
+        private async static Task<PassOut> PassOutAsync()
+        {
+            Console.WriteLine("Cathy, I'm not feeling too well");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine("The water was too strong, I think I need to lie down");
+            return new PassOut();
         }
 
         private static void PlayPartyGames()
@@ -52,4 +82,21 @@ namespace AysncCake
             return "Here's a cake";
         }
     }
+
+    public class Drinks
+    {
+        public override string ToString()
+        {
+            return "Here's a cup of water for everyone";
+        }
+    }
+
+    public class PassOut
+    {
+        public override string ToString()
+        {
+            return "ZZZZzzz";
+        }
+    }
+
 }
