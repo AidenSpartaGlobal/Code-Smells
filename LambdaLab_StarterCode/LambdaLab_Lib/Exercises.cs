29cb19b386e48203e56c69d5305835f5500f9b55
AidenSpartaGlobal <Skwok@spartaglobal.com>
2023-05-03 10:31:40 +0100

    lab update

--- LambdaLab_StarterCode/LambdaLab_Lib/Exercises.cs
+++ LambdaLab_StarterCode/LambdaLab_Lib/Exercises.cs
@@ -1,4 +1,6 @@
-﻿namespace LambdaLab_Lib
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LambdaLab_Lib
 {
     public class Exercsises
     {
@@ -11,17 +13,19 @@
         public static int NumberOfSpartansBornAfter1980(List<Spartan> spartans) => spartans.Count(x => x.DateOfBirth.Year > 1980);
 
 
-        public static int SumOfAllSpartaMarksMoreThan50Inclusive(List<Spartan> spartans) => spartans.Where(x => x.SpartaMark >= 50).Select(x => x.SpartaMark).Sum();
+        public static int SumOfAllSpartaMarksMoreThan50Inclusive(List<Spartan> spartans) => spartans.Where(x => x.SpartaMark >= 50).Sum(x => x.SpartaMark);
+                                                                                           //spartans.Where(x => X.SpartaMark >= 50).Select(x => x.SpartaMark).Sum();  
 
         //To 2 decimal points
         public static double AverageSpartanMark(List<Spartan> spartans) => Math.Round(spartans.Average(x => x.SpartaMark), 2);
-
-
+                                                                          //spartans.Average(x => x.SpartaMark.Sum());
+                                                                                
 
         public static List<Spartan> SortByAlphabeticallyByLastName(List<Spartan> spartans) => spartans.OrderBy(x => x.LastName).ToList();
-
+                                                                                        //spartans.OrderBy(x => x.LastName);
 
         public static List<string> ListAllDistinctCities(List<Spartan> spartans) => spartans.Select(x => x.City).Distinct().ToList();
+                                                                //spartans.Where(x => x.city).Select(x => x.City.Distinct()).ToList();
 
 
         public static List<Spartan> ListAllSpartansWithIdBeginingWithB(List<Spartan> spartans) => spartans.FindAll(x => x.SpartanId.StartsWith("B"));
