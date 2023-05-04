effbb0dbdf9906e83588a666fdc870b451bfd60e
AidenSpartaGlobal <Skwok@spartaglobal.com>
2023-05-04 11:40:22 +0100

    serialisation

---
+++ SerialisationApp/Model.cs
@@ -0,0 +1,36 @@
+﻿
+namespace SerialisationApp
+{
+    [Serializable]
+    public class Trainee
+    {
+        public string? FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = "";
+        public int? SparaNo { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public override string ToString()
+        {
+            return $"{SparaNo} - {FullName}";
+        }
+    }
+
+    [Serializable]
+    public class Course
+    {
+        public string Subject { get; set; }
+        public string Title { get; set; }
+        public DateOnly StartDate { get; set; }
+        public List<Trainee> Trainees { get; } = new List<Trainee>();
+        [field: NonSerialized]
+        private readonly DateOnly _lastRead;
+        public Course()
+        {
+            _lastRead = DateOnly.FromDateTime(DateTime.Now);
+        }
+        public void AddTrainee(Trainee newTrainee)
+        {
+            Trainees.Add(newTrainee);
+        }
+    }
+}
+
---
+++ SerialisationApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SerialisationApp
+{
+
+    internal class Program
+    {
+        private static ISerialise _serialiser;
+        private static string _path = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"); 
+
+        static void Main(string[] args)
+        {
+            Trainee trainee = new Trainee
+            {
+                FirstName = "Joseph",
+                LastName = "McCann",
+                SparaNo = 100
+            };
+
+            _serialiser = new SerialiserXML();
+            _serialiser.SerialiseToFile(_path + "XMLTrainee.xml", trainee);
+
+            Trainee deserialisedXMLTrainee = _serialiser.DeserialisationFromFile<Trainee>(_path + "XMLTrainee.xml");
+
+            Course Tech212 = new Course { Title = "TECH 212", Subject = "C# Test", StartDate = new DateOnly(2023, 3, 20) };
+
+            Tech212.AddTrainee(trainee);
+            Tech212.AddTrainee(new Trainee { FirstName = "Anthony", LastName = "Naguib", SparaNo = 101});
+            _serialiser.SerialiseToFile(_path + "XMLCourse.xml", Tech212);
+
+            var editedCourse = _serialiser.DeserialisationFromFile<Course>(_path + "XMLCourseedited.xml");
+        }
+    }
+}
